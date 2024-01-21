8ce5f3996dae275e1507c365262f35c051b88b79
Ali Nacafov <alinacaf10@gmail.com>
2024-01-21 14:38:36 +0400

    add CorsConfig

---
+++ src/main/java/az/edu/university/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package az.edu.university.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500") // İzin vermek istediğiniz origin'i buraya ekleyin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600); // Opsiyonel: Preflight request'lerin max age değeri
+    }
+}
